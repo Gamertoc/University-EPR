80fadda3fd6a7e1f556d0847e3aa4403c113b23d
Gamertoc <33159593+Gamertoc@users.noreply.github.com>
2019-10-21 10:39:14 +0200

    Update codecorrections.py

--- Tasks_00/codecorrections.py
+++ Tasks_00/codecorrections.py
@@ -3,8 +3,8 @@ __author__ = "Oliver Theobald, 7146127"
 current_time = int(input("What is the current time (in hours 0-23)? "))
 wait_time = int(input("How many hours do you want to wait? "))
 final_time = current_time + wait_time
-print("Waiting will end at", final_time % 24, "o' clock.")
+print("The time is now", final_time % 24)
 
 n = int(input("What time is it now (in hours)? "))
 m = int(input("How many hours do you want to wait? "))
-print("The time is now", (n+m) % 24, "o' clock.")
+print("The time is now", (n+m) % 24)
