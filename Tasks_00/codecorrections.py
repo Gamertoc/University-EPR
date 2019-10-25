629e9f5c4d88c4bd1588b00993919a4db682eaed
Gamertoc <33159593+Gamertoc@users.noreply.github.com>
2019-10-25 21:31:02 +0200

    Update codecorrections.py

--- Tasks_00/codecorrections.py
+++ Tasks_00/codecorrections.py
@@ -1,4 +1,9 @@
 __author__ = "Oliver Theobald, 7146127"
+# General rules:
+# 1) The code has to be accepted by python
+# 2) The code has to follow our conventions
+# 3) In every instance where a variable is named the wrong way, I assume that by changing the original name
+# all other instances of this variable are changed accordingly, and therefore won't write them up as errors. 
 # PART A
 # current_time_str = input(,"What is the current time (in hours 0-23)?")
 # Error 1: the datatype doesn't belong in the name 
@@ -20,11 +25,11 @@ current_time = int(current_time)
 wait_time = int(wait_time)
 
 # final_time_int = current_time_int + wait_time_int
-# no errors
+# Error 1
 final_time = current_time + wait_time
 
 # print(final_time_int)
-# no errors
+# Error 5: Without modulo 24 the function wouldn't make much sense since there can't be a time like 30 o' clock
 print("The time is now", final_time % 24)
 
 
@@ -34,7 +39,7 @@ print("The time is now", final_time % 24)
 n = input("What time is it now (in hours)?")
 
 # n = imt(n)
-# Error 5: spelling. The function is called int and not imt
+# Error 6: spelling. The function is called int and not imt
 n = int(n)
 
 # m = input("How many hours do you want to wait?")
@@ -46,6 +51,6 @@ m = input("How many hours do you want to wait?")
 m = int(m)
 
 # prunt("The time is now", m % 12)
-# Error 5
-# Error 6: m % 12 doesn't make very much sense since it doesn't add the current time. It has to be (n+m) % 12
+# Error 6
+# Error 7: m % 12 doesn't make very much sense since it doesn't add the current time. It has to be (n+m) % 12
 print("The time is now", (n+m) % 12)
