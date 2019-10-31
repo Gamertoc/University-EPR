8d8594898ebe81d3c0abe372d2c628b8e4f97f44
Gamertoc <oliver@theobald-united.de>
2019-10-31 14:50:04 +0100

    Test cases
    Signed-off-by: Gamertoc <oliver@theobald-united.de>

--- Tasks_01/tendency.py
+++ Tasks_01/tendency.py
@@ -4,7 +4,7 @@ __author__ = "Oliver Theobald, 7146127"
 # We assume that the input is an integer within the range of 0-15
 n = int(input("Please enter a grade in points (0-15): "))
 
-# I use this array to simply determine the according grade
+# I use this array to simply determine the according grades based on how we used them in school
 grades = {
     15: "1+",
     14: "1",
@@ -24,8 +24,16 @@ grades = {
     0: "6"
 }
 
-print("The grade ", n, " equals ", grades.get(n), ". ",  sep="", end="")
+# In my school you pass the test when you have at least 5 points. Since the same goes for the
+# Abitur, I'm gonna use this
+print(n, " points equals ", grades.get(n), ". ",  sep="", end="")
 if n >= 5:
     print("The user passed the test.")
 else:
     print("The user failed the test.")
+
+# Test cases (as always Input : Output)
+# 0 : 0 points equals 6. The user failed the test.
+# 15 : 15 points equals 1+. The user passed the test.
+# 10 : 10 points equals 2-. The user passed the test.
+# 4 : 4 points equals 4-. The user failed the test.
