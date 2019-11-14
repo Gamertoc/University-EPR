45a0495d3ae3bc782fe6fde17817639a2596521d
Gamertoc <oliver@theobald-united.de>
2019-11-14 12:15:04 +0100

    task 2
    Signed-off-by: Gamertoc <oliver@theobald-united.de>

--- Tasks03/controlstructures.py
+++ Tasks03/controlstructures.py
@@ -3,27 +3,59 @@
 __author__ = "7146127, Theobald"
 
 
-def increment(loops, step):
+def increment(loops, step, x_start = 7):
     """
 
+    :param loops: int
+    :param step: int or float
+    :param x_start: int or float
+    :return: None
     """
-    x = 7
-    string = ""
+    x = x_start
+    prev_numbers = ""
     for i in range(loops):
-        string += " " + str(x)
+        if x == int(x):
+            x = int(x)
+        prev_numbers += " " + str(x)
         x += step
         print("Now, x is ", x)
-        print("All previous numbers: ", string)
+        print("All previous numbers: ", prev_numbers)
 
 
 def main():
-    """
+    """Running the program at start."""
 
-    """
-    step = int(input("Please enter the step value: "))
-    loops = int(input("Please enter the number of loops: "))
-    increment(loops, step)
+    # We need to make sure that we get valid input values
+    while True:
+        step = input("Please enter the step value (can be int or float): ")
+        try:
+            step = float(step)
+            break
+        except ValueError:
+            continue
 
+    # Same for the loop value
+    while True:
+        loops = input("Please enter the number of repetitions (int): ")
+        try:
+            loops = int(loops)
+            break
+        except ValueError:
+            continue
+
+    # You can decide if you want to set a new start value (standard is 7)
+    if input("Do you want a different start value than 7? (Y/n) ") == "Y":
+        # If you want, we have to make sure that the value is valid
+        while True:
+            x_start = input("Please enter the new starting value (int or float): ")
+            try:
+                x_start = float(x_start)
+                break
+            except ValueError:
+                continue
+        increment(loops, step, x_start)
+    else:
+        increment(loops, step)
 
 if __name__ == '__main__':
     main()
