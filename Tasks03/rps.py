905bc953c4fca0971970bbcb59ecd262e87fc18f
Gamertoc <oliver@theobald-united.de>
2019-11-16 03:12:01 +0100

    Rock, Paper, Scissors
    Signed-off-by: Gamertoc <oliver@theobald-united.de>

--- Tasks03/rps.py
+++ Tasks03/rps.py
@@ -1,31 +1,52 @@
-"""This program lets you play rock-paper-scissors against a machine. Since I wanna do some good
-work, I will program different methods of playing first, then let them play against each other
-and then let the best one play against you lul."""
+"""This program lets you play rock-paper-scissors against a machine.
+Since I wanna do some good work, I will program different methods of
+playing first, then let them play against each other and then let the
+best one play against you lul.
+"""
 
 __author__ = "7146127, Theobald"
 
 import random as dice
 
 
+def rock():
+    """Just returning Rock, nothing else to see here"""
+    return "Rock"
+
+
+def paper():
+    """Just returning Paper"""
+    return "Paper"
+
+
+def scissors():
+    """Just returning Scissors"""
+    return "Scissors"
+
+
 def play_random():
-    """This function just randomly decides what it wants to play and doesn't take anything into
-    account."""
+    """This function just randomly decides what it wants to play and
+    doesn't take anything into account.
+    """
 
     rps = dice.randint(1, 3)
     if rps == 1:
-        return "Rock"
+        return rock()
     elif rps == 2:
-        return "Paper"
+        return paper()
     else:
-        return "Scissors"
+        return scissors()
 
 
 def play_human(last_game_pick, last_game_result):
-    """Humans tend to keep a winning strategy, but tend to switch when they lose. Source:
-    Experience and Google (https://arxiv.org/pdf/1404.5199v1.pdf). It is also called the
-    "win-stay, lose-shift" strategy. That's exactly what this one will do. For this one the
-    program will be written in a way that it gets its last pick and the result of that. If it
-    was successful, it will stick with the decision, else it will change.
+    """Humans tend to keep a winning strategy, but tend to switch when
+    they lose.
+    Source: Experience and https://arxiv.org/pdf/1404.5199v1.pdf.
+    It is also called the "win-stay, lose-shift" strategy. That's
+    exactly what this one will do. For this one the program will be
+    written in a way that it gets its last pick and the result of that.
+    If it was successful, it will stick with the decision, else it will
+    change.
     :param last_game_pick: String
     :param last_game_result: String
     :return: String
@@ -38,8 +59,9 @@ def play_human(last_game_pick, last_game_result):
     # If we won the last game we will stick with the decision.
     if last_game_result == "win":
         return last_game_pick
-    # In any other case, we will switch. For that we use the random class to determine our
-    # strategy as long as it's different from the last one.
+    # In any other case, we will switch. For that we use the random
+    # class to determine our strategy as long as it's different from the
+    # last one.
     while True:
         rps = play_random()
         if rps != last_game_pick:
@@ -47,40 +69,50 @@ def play_human(last_game_pick, last_game_result):
 
 
 def play_antihuman(last_game_pick, last_game_result):
-    """This strategy is a strategy devised from the human behavior mentioned above. An article
-    on the website arstechnica (https://arstechnica.com/science/2014/05/win-at-rock-paper
-    -scissors-by-knowing-thy-opponent/) describes is this way: If you lose, switch to the thing
-    that beats the thing your opponent just played. If you win, don't keep playing the same
-    thing, but instead switch to the thing that would beat the thing that you just played.
+    """This strategy is a strategy devised from the human behavior
+    mentioned above. An article on the website arstechnica
+    (https://arstechnica.com/science/2014/05/win-at-rock-paper-scissors
+    -by-knowing-thy-opponent/) describes is this way: If you lose,
+    switch to the thing that beats the thing your opponent just played.
+    If you win, don't keep playing the same thing, but instead switch to
+    the thing that would beat the thing that you just played.
+    :param last_game_pick: String
+    :param last_game_result: String
+    :return: String
     """
 
     # For the first game there is no strategy, so it just plays random
     if last_game_pick == "None":
         return play_random()
 
-    # If we won the last game, we will play whatever would beat that what we just played.
+    # If we won the last game, we will play whatever would beat that
+    # what we just played.
     if last_game_result == "win":
         if last_game_pick == "Rock":
-            return "Paper"
+            return paper()
         elif last_game_pick == "Paper":
-            return "Scissors"
+            return scissors()
         else:
-            return "Rock"
+            return rock()
 
-    # If we lost, we will play whatever beats the thing our opponent just played.
+    # If we lost, we will play whatever beats the thing our opponent
+    # just played.
     if last_game_pick == "Rock":
-        return "Scissors"
+        return scissors()
     elif last_game_pick == "Paper":
-        return "Rock"
+        return rock()
     else:
-        return "Paper"
+        return paper()
 
 
 def check_win(pick_one, pick_two):
-    """This function checks which of the picks won. A draw counts as loss for both, since nobody
-    gets points in a draw.
+    """This function checks which of the picks won. A draw counts as
+    loss for both, since nobody gets points in a draw.
+    :param pick_one: String
+    :param pick_two: String
+    :return: String
     """
-    # We just check which thing won
+    # We just check which thing won and return it accordingly
     if pick_one == "Rock":
         if pick_two == "Paper":
             return "Second won"
@@ -100,11 +132,12 @@ def check_win(pick_one, pick_two):
 
 
 def find_best_machine(revisions):
-    """We wanna find out what the best algorithm is. For this we let every algorithm play
-    against every other.
+    """We wanna find out what the best algorithm is. For this we let
+    every algorithm play against every other.
     :param revisions: int
     """
-    # The first one mentioned is the one that counts, the second one is the enemy
+    # The first one mentioned is the one that counts, the second one is
+    # the enemy
     random_human = 0
     random_antihuman = 0
     human_random = 0
@@ -112,15 +145,22 @@ def find_best_machine(revisions):
     antihuman_random = 0
     antihuman_human = 0
 
-    # Let's start with the game random vs human
+    # Let's start with the game random vs human. And since we basically
+    # do the same stuff four times (three times in bot vs bot and then
+    # for bot vs human again), I'll just explain it one time
     human_last_pick = "None"
     human_last_result = "None"
     i = 0
-    # Draws don't count to the counter
+    # Draws don't count to the counter, so we use a while-loop and just
+    # increase the counter when someone won.
     while i < revisions:
+        # We let both algorithms play and let the function check who won
         human = play_human(human_last_pick, human_last_result)
         random = play_random()
         result = check_win(random, human)
+
+        # Depending on who won we adjust the counter and the feedback
+        # for the last result
         if result == "First won":
             human_random += 1
             human_last_result = "loss"
@@ -131,14 +171,19 @@ def find_best_machine(revisions):
             i += 1
         else:
             human_last_result = "loss"
+
+        # Don't forget the last pick for the functions that need it
         human_last_pick = human
 
+    # And after all that print a nice little message about the score and
+    # who won.
     if human_random > random_human:
         print("Human won vs random in", revisions, "rounds with", human_random, "-", random_human)
     else:
         print("Random won vs human in", revisions, "rounds with", random_human, "-", human_random)
 
-    # Now it's the same for the other two games. Starting with random vs antihuman
+    # Now it's the same for the other two games. Starting with random vs
+    # antihuman
     antihuman_last_pick = "None"
     antihuman_last_result = "None"
     i = 0
@@ -200,7 +245,8 @@ def find_best_machine(revisions):
         print("Antihuman won vs human in", revisions, "rounds with", antihuman_human, "-",
               human_antihuman)
 
-    # Now we add all the games together and get a total for games won each
+    # Now we add all the games together and get a total for which
+    # funcion won how many games
     antihuman_won_games = antihuman_human + antihuman_random
     human_won_games = human_antihuman + human_random
     random_won_games = random_human + random_antihuman
@@ -208,11 +254,13 @@ def find_best_machine(revisions):
     print("Antihuman won a total of", antihuman_won_games, "games.")
     print("Random won a total of", random_won_games, "games.")
     print("Antihuman won with", antihuman_won_games, "out of", 2 * revisions, "games.")
-    # Antihuman proved to be the winning strategy, since it was literally designed to win
-    # against average humans. Therefore it will always be the winner in this contests.
+    # Antihuman proved to be the winning strategy, since it was
+    # literally designed to win against average humans. Therefore it
+    # will always be the winner in this contests.
 
 
 def main():
+    """Starting the program."""
     if input("Do you want to let the bots play against each other? (Y/n) ") == "Y":
         find_best_machine(1000000)
     antihuman_last_pick = "None"
@@ -222,11 +270,20 @@ def main():
     while True:
         user = input("Do you want to throw Rock, Paper or Scissors? ")
         antihuman = play_antihuman(antihuman_last_pick, antihuman_last_result)
+
+        # You can quit by entering q. It will show you the score you
+        # achieved
         if user == "q":
             print("You played", user_count, "-", antihuman_count)
             exit()
+
+        # If the input is neither q nor a valid game input, you just
+        # have to enter something valid
         elif not user == "Scissors" and not user == "Rock" and not user == "Paper":
             continue
+
+        # Same as in the bot vs bot version: Adjusting the win counter
+        # and the last_pick/last_result for the machine
         else:
             result = check_win(user, antihuman)
             if result == "First won":
@@ -245,3 +302,18 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# TfTC:
+# Y, q:
+# Random won vs human in 1000000 rounds with 500414 - 499586
+# Antihuman won vs random in 1000000 rounds with 500442 - 499558
+# Antihuman won vs human in 1000000 rounds with 1000000 - 0
+# Human won a total of 499586 games.
+# Antihuman won a total of 1500442 games.
+# Random won a total of 999972 games.
+# Antihuman won with 1500442 out of 2000000 games.
+# Do you want to throw Rock, Paper or Scissors? q
+# You played 0 - 0
+
+# Now some actual games.
+#
