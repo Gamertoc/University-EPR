4a74a2dc80e175fb33229128e477bd158e7a46de
Gamertoc <oliver@theobald-united.de>
2020-02-04 19:41:28 +0100

    Changing rows and cols via function
    Signed-off-by: Gamertoc <oliver@theobald-united.de>

--- Tasks07/battleships.py
+++ Tasks07/battleships.py
@@ -4,19 +4,43 @@ __author__ = "7146127, Theobald"
 __credits__ = "Water"
 __email__ = "s7223152@cs.uni-frankfurt.de"
 
-import array
-
 
 class Game:
 
-    def main(self):
+    def __init__(self):
+        """We need some things in every game."""
+        self.rows = 10
+        self.cols = 10
+
+    def setup(self):
+        """We need to set up the game, in case you want to change some
+        settings.
+        :return: None
+        """
         b = Board()
-        b.create_board()
+        b.create_board(self.rows, self.cols)
+
+    def change_rows(self, x):
+        """This lets you change the number of rows.
+        :param x: int
+        :return: None
+        """
+        self.rows = x
+
+    def change_cols(self, y):
+        """This lets you change the number of columns.
+        :param y: int
+        :return: None
+        """
+        self.cols = y
 
 
 class Board:
 
-    def create_board(self, cols=5, rows=10):
+    def __init__(self):
+        self.board = []
+
+    def create_board(self, rows, cols):
         """In this function the board is created. The standard size is
         10x10, but this can be changed.
         :param cols: int
@@ -24,15 +48,12 @@ class Board:
         :return: None
         """
 
-        # The board is a 2D-Array.
-
-        # Every element of the board is a field which has the according
-        # x and y values.
-        board = []
+        # The board is a 2D-Array. Every element of the board is a field
+        # which has the according x and y values.
         for i in range(rows):
-            board.append([])
+            self.board.append([])
             for j in range(cols):
-                board[i].append(Field(i, j))
+                self.board[i].append(Field(i, j))
 
 
 class Field:
@@ -40,8 +61,9 @@ class Field:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.value = "empty"
 
 
 if __name__ == "__main__":
     game = Game()
-    game.main()
+    game.setup()
