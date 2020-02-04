283ccf017e28c325f2e483a9f8d0d38b82a7f721
Gamertoc <oliver@theobald-united.de>
2020-02-04 23:14:51 +0100

    Generating a random combination of ships
    Signed-off-by: Gamertoc <oliver@theobald-united.de>

--- Tasks07/battleships.py
+++ Tasks07/battleships.py
@@ -4,6 +4,8 @@ __author__ = "7146127, Theobald"
 __credits__ = "Water"
 __email__ = "s7223152@cs.uni-frankfurt.de"
 
+import random as rng
+
 
 class Game:
 
@@ -17,7 +19,7 @@ class Game:
         self.spray = 15
         self.shots_per_ship = False
         self.players = []
-        self.random_ships = False
+        self.random_ship_combination = False
         self.ships = []
 
     def setup(self):
@@ -62,8 +64,72 @@ class Game:
         """
         pass
 
-    def random_ships(self):
-        """This """
+    def random_ship_combination(self):
+        """This function gives you a random combination of ships.
+        :return: None
+        """
+        # We reset the ships in case there are any rests from previous
+        # games
+        # A fleet can take between 10% and 25% of the board. We don't
+        # want decimal numbers, only integers.
+        field_count = self.rows * self.cols
+        fleet_max = (field_count / 4) // 1
+        fleet_min = field_count / 10
+        if not fleet_min == fleet_min // 1:
+            fleet_min = fleet_min // 1 + 1
+
+        # Now we take a random value between the min and the max to
+        # determine the size of our fleet. It must be at least 6 since
+        # we need at least 2 ships that are at least 3 spaces long.
+        while True:
+            fleet_size = rng.randint(fleet_min, fleet_max)
+            if fleet_size >= 6:
+                break
+
+        # Now we get to the algorithm (more on that in the
+        # documentation
+        while True:
+
+            # If the remaining fleet is smaller than 8 (except for 6),
+            # we can determine the other ships and escape afterwards
+            if fleet_size == 7:
+                self.ships.append(4)
+                self.ships.append(3)
+                break
+            elif fleet_size < 6:
+                self.ships.append(fleet_size)
+                break
+
+            # If the fleet has a size of 6, there are 2 combinations.
+            elif fleet_size == 6:
+                ship_count = rng.randint(1, 2)
+                if len(self.ships) == 0 or ship_count == 2:
+                    self.ships.append(3)
+                    self.ships.append(3)
+                else:
+                    self.ships.append(6)
+                break
+
+            # If the fleet is bigger, we use a simple algorithm
+            else:
+
+                # First, we need to check for the largest ship that we
+                # can have while having another ship after that.
+                largest_possible = 6
+                while True:
+                    if fleet_size - largest_possible < 3:
+                        largest_possible -= 1
+                    else:
+                        break
+
+                # Now we generate a ship,
+                ship = rng.randint(3, largest_possible)
+                # add it to the other ones
+                self.ships.append(ship)
+                # and reduce the fleet size.
+                fleet_size -= ship
+
+        print(self.ships)
 
 
 class Player:
